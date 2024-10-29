00b8c29398d0cb3c47152f628a82a403e6794363
Timothy Walkiden <walkidentimothy@gmail.com>
2024-10-29 16:30:46 +0000

    wrong date

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <Hero title="Tim Walkiden" subHeading="Frontend web developer"
       missionStatement="Enhancing the web's beauty, one pixel at a time" />
     <TwoColsWithImg title="About Me" img="profilePic.webp" :description="[
-      'I first discovered web development while pursuing my undergraduate degree in computer science in 2020, which led me to shift my focus to web design and development.',
+      'I first discovered web development while pursuing my undergraduate degree in computer science in 2015, which led me to shift my focus to web design and development.',
       ' I have a solid foundation in HTML, CSS, and JavaScript/TypeScript, with practical experience in modern front-end frameworks like React and Vue. I emphasize best practices to deliver clean, maintainable, and efficient code.',
       ' With a strong focus on building seamless and impactful user interfaces, and a deep interest in UX design, I strive to meet technical requirements while ensuring that the UI is intuitive, accessible, and visually appealing.'
 
